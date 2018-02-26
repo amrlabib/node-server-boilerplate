25e06d211dfd9f9ac2db6eb9ff491ecaec2a7c1e
Amr Labib <amaly@noon.com>
2018-02-26 11:13:57 +0400

    add body parser and log query and body

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const config = require('./config');
-
+const bodyParser = require('body-parser')
 const debug = require('debug')('server');
 
+app.use(bodyParser.json());
+
 app.use( (req,res,next) => {
+	debug(`----------------${req.url}----------------`);
 	debug(`Request method: ${req.method}`);
+	debug(`Request url: ${req.url}`);
+	debug(`Request query: ${JSON.stringify(req.query)}`);
+	debug(`Request body: ${JSON.stringify(req.body)}`);
+	debug('------------------------------------');
 	next();
 });
 
