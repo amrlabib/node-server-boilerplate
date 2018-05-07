b1eae302f6549d02086be4a4a104dc00bcb29a9e
Amr Labib <amr.h.labib@gmail.com>
2018-05-08 00:40:21 +0400

    allow origin header

--- server/config/index.js
+++ server/config/index.js
@@ -9,5 +9,7 @@ module.exports = {
     },
     responseHeaders: {
         'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     },
 }
